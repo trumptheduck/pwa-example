021ac245ff6bc88b6a6fe807501ab035b66194f9
Nhat <nhatyt123@gmail.com>
2021-05-06 00:02:58 +0700

    first commit

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -13,32 +13,6 @@ self.addEventListener("message", (event) => {
     self.skipWaiting();
   }
 });
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => {
-        return caches.open(HTML_CACHE)
-          .then((cache) => {
-            return cache.match(event.request)
-          })
-      })
-  )
-})
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys()
-      .then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-          if (key !== HTML_CACHE) {
-            console.log('[ServiceWorker] Removing old cache', key)
-            return caches.delete(key)
-          }
-        }))
-      })
-      .then(() => self.clients.claim())
-  )
-})
 workbox.routing.registerRoute(
   ({event}) => event.request.destination === 'document',
   new workbox.strategies.NetworkFirst({
